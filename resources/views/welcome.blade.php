15cec9a7053f83f53ebbf6dd6a4d6ba40807fba5
ibrasim <asim.19824@gmail.com>
2024-09-08 21:45:50 +0500

    sdf

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,8 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     @vite('resources/css/app.css')
+  
 </head>
 <body>
+
+
 <h1 class="p-10 border-b-8 border-indigo-900 text-3xl font-bold  text-sky-400">
     Asims newsletter 1 -13 September 2024--...
   </h1>
@@ -14,19 +17,24 @@
   <h1 class="p-10 py-1 px-3 text-orange-400  text-3xl font-bold" >
    Economy
   </h1>
+  
   <h1 class="p-10 py-1 px-3  text-rose-500  text-3xl font-bold" >
    Digital Economy
   </h1>
 
-<div class="m-8 p-8 border-2 border-gray-500 bg-red-300">
+<div class="m-8 p-8 border-2 border-gray-500 bg-red-300 w-96">
     <h1 class="text-2xl mb-4">Tailwind CSS Example</h1>
     <p class="mb-2">This is a paragraph with some padding and margin.</p>
     <button class="bg-blue-500 text-white p-2 rounded">Click Me</button>
+    
 </div>
 
 
+
 <p class=" p-10 text columns-3xl font-mono size-48 text-rose-500"  > dsfsdfsdfsfd</p>
-  
+
+
+
 </body>
 
 </html>
